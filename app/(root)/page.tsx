c0b9659967123f37fc5c88acd68f163ba81b980a
陶浩南 <14639548+haonantao-aaron@user.noreply.gitee.com>
2025-01-20 19:04:25 +0800

    clerk signin signup works!
    clerk signin signup works!

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,11 @@
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <>
       <main>
         <h1>Threads</h1>
+        2<UserButton />
       </main>
     </>
   );
