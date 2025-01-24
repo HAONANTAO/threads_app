e622fa2e3653e7cd127b7e8b8fc3947b7b9d651f
陶浩南 <14639548+haonantao-aaron@user.noreply.gitee.com>
2025-01-24 21:12:07 +0800

    start accountprofile
    end2025.01.24

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,3 +1,5 @@
+import AccountProfile from "@/components/forms/AccountProfile";
+
 async function Page() {
   return (
     <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
@@ -6,7 +8,9 @@ async function Page() {
         Complete your profile now to use Threads
       </p>
 
-      <section className="mt-9 bg-dark-2 p-10"></section>
+      <section className="mt-9 bg-dark-2 p-10">
+        <AccountProfile/>
+      </section>
     </main>
   );
 }
---
+++ components/forms/AccountProfile.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AccountProfile = () => {
+  return (
+    <>
+      <div>AccountProfile</div>
+    </>
+  );
+}
+
+export default AccountProfile
