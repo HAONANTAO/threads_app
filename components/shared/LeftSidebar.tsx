140747da7c8938377acdcebc134104608d31ef2e
陶浩南 <14639548+haonantao-aaron@user.noreply.gitee.com>
2025-01-24 14:35:09 +0800

    active link check
    active link check

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,27 +1,39 @@
+"use client";
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
 // @ == root
 import { sidebarLinks } from "@/constants/index";
 const LeftSidebar = () => {
+  const router = useRouter();
+  const pathname = usePathname();
   return (
     <section className="custom-scrollbar leftsidebar">
       <div className="flex w-full flex-1 flex-col gap-6 px-6">
-        {sidebarLinks.map((link) => (
-          <div>
-            <Link
-              href={link.route}
-              key={link.label}
-              className="leftsidebar_link">
-              <Image
-                src={link.imgURL}
-                alt={link.label}
-                width="24"
-                height="24"/>
+        {/* constants map iterations */}
+        {sidebarLinks.map((link) => {
+          // active link check，part of or full equal
+          const isActive =
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname === link.route;
+          return (
+            <div>
+              <Link
+                href={link.route}
+                key={link.label}
+                className={`leftsidebar_link ${isActive && "bg-primary-500"}`}>
+                <Image
+                  src={link.imgURL}
+                  alt={link.label}
+                  width="24"
+                  height="24"
+                />
                 <p className="text-light-1 max-lg:hidden">{link.label}</p>
-            </Link>
-          </div>
-        ))}
+              </Link>
+            </div>
+          );
+        })}
       </div>
     </section>
   );
