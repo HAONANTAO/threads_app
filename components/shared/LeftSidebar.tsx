954756c2d975642f8813e5bfc44248da2a46a2cd
陶浩南 <14639548+haonantao-aaron@user.noreply.gitee.com>
2025-01-24 13:22:54 +0800

    sidebar icon show
    sidebar icon show

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,10 +1,27 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
+// @ == root
+import { sidebarLinks } from "@/constants/index";
 const LeftSidebar = () => {
   return (
     <section className="custom-scrollbar leftsidebar">
       <div className="flex w-full flex-1 flex-col gap-6 px-6">
-        <Link></Link>
+        {sidebarLinks.map((link) => (
+          <div>
+            <Link
+              href={link.route}
+              key={link.label}
+              className="leftsidebar_link">
+              <Image
+                src={link.imgURL}
+                alt={link.label}
+                width="24"
+                height="24"/>
+                <p className="text-light-1 max-lg:hidden">{link.label}</p>
+            </Link>
+          </div>
+        ))}
       </div>
     </section>
   );
