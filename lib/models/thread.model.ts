a7d4ec48b50e2c92c7183f4d046852e847efe93f
陶浩南 <14639548+haonantao-aaron@user.noreply.gitee.com>
2025-02-07 16:47:11 +0800

    model down

--- lib/models/thread.model.ts
+++ lib/models/thread.model.ts
@@ -1,48 +1,19 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  id: {
-    type: String,
-    required: true,
-  },
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  name: {
-    type: String,
-    required: true,
-  },
-  image: {
-    type: String,
-  },
-  bio: {
-    type: String,
-  },
-  // important one
-  threads: [
-    // 通常用作文档的唯一标识符。
+const threadSchema = new mongoose.Schema({
+  text: { type: String, required: true },
+  author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  community: { type: mongoose.Schema.Types.ObjectId, ref: "Community" },
+  createdAt: { type: Date, default: Date.now },
+  parentId: { type: String },
+  // recursion for comment
+  children: [
     {
       type: mongoose.Schema.Types.ObjectId,
-      // threads 数组中的每个 ObjectId 都指向一个 Thread 模型中的文档。
       ref: "Thread",
     },
   ],
-  onboarded: {
-    type: Boolean,
-    default: false,
-  },
-  communities: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Community",
-    },
-  ],
 });
+const Thread = mongoose.models.Thread || mongoose.model("Thread", threadSchema);
 
-// 第一个为了二次使用方便
-// 创建 User 模型
-const User = mongoose.models.User || mongoose.model("User", userSchema);
-
-export default User;
+export default Thread;
