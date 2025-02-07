fcf35a90c9d6cecf0c95cff5f343502a73e0477b
陶浩南 <14639548+haonantao-aaron@user.noreply.gitee.com>
2025-02-07 16:50:46 +0800

    createThread

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -1,4 +1,6 @@
+import { revalidatePath } from "next/cache";
 import Thread from "../models/thread.model";
+import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
 
 interface Params {
@@ -14,9 +16,18 @@ export async function createThread({
   path,
 }: Params) {
   connectToDB();
-  const createdThread = await Thread.create
+  // TODO:communityId
+  const createdThread = await Thread.create({ text, author, community: null });
+
+  // update user model(for thread)
+  await User.findByIdAndUpdate(author, {
+    $push: { threads: createdThread._id },
+  });
+  // update community model
+
+  revalidatePath(path);
   try {
   } catch (error: any) {
-    throw new Error(`error: ${error.message}`);
+    throw new Error(`createThread error: ${error.message}`);
   }
 }
