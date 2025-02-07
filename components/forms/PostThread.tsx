a9292b4a96c144a437e930d8d81a033fbaee017f
陶浩南 <14639548+haonantao-aaron@user.noreply.gitee.com>
2025-02-07 16:11:17 +0800

    basic thread post

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -15,31 +15,17 @@ import { z } from "zod";
 
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { UserValidation } from "@/lib/validations/user";
-import Image from "next/image";
 import { Textarea } from "../ui/textarea";
-
-import { updateUser } from "@/lib/actions/user.actions";
 import { usePathname, useRouter } from "next/navigation";
+import { ThreadValidation } from "@/lib/validations/thread";
 
-interface Props {
-  user: {
-    id: string;
-    objectId: string;
-    username: string | null | undefined;
-    name: string;
-    bio: string;
-    image: string | undefined;
-  };
-  btnTitle: string;
-}
 
 const PostThread = ({ userId }: { userId: string }) => {
   const router = useRouter();
   const pathname = usePathname();
 
   const form = useForm({
-    resolver: zodResolver(),
+    resolver: zodResolver(ThreadValidation),
     defaultValues: {
       thread: "",
       accountId: userId,
--- lib/validations/thread.ts
+++ lib/validations/thread.ts
@@ -1,6 +1,10 @@
 // import * as z from "zod";
 import { z } from "zod";
 export const ThreadValidation = z.object({
-  thread: z.string().nonempty().min(3, { message: "minimun 3 characters" }),
+  thread: z.string().nonempty().min(3, { message: "minimum 3 characters" }),
   accountId: z.string(),
+});
+
+export const CommentValidation = z.object({
+  thread: z.string().nonempty().min(3, { message: "minimum 3 characters" }),
 });
