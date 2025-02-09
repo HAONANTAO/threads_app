b4e2f731a5955ee6b336f156ad8f8c2f359ba76a
陶浩南 <14639548+haonantao-aaron@user.noreply.gitee.com>
2025-02-09 14:23:16 +0800

    create the comment part

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -33,6 +33,7 @@ const ThreadCard = ({
   community,
   createdAt,
   comments,
+  isComment,
 }: Props) => {
   return (
     <>
@@ -103,6 +104,14 @@ const ThreadCard = ({
                     className="cursor-pointer object-contain"
                   />
                 </div>
+                {/* 这个如果有comment */}
+                {isComment && comments.length > 0 && (
+                  <Link href="{`/thread/${id}`}">
+                    <p className="mt-1 text-subtle-medium text-gray-1">
+                      {comments.length} replies{" "}
+                    </p>
+                  </Link>
+                )}
               </div>
             </div>
           </div>
