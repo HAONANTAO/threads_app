998f213a0c27b7900e540314d1c076c419fda304
陶浩南 <14639548+haonantao-aaron@user.noreply.gitee.com>
2025-02-10 10:31:40 +0800

    end with create thread page and comment page

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -35,7 +35,7 @@ const page = async ({ params }: { params: { id: string } }) => {
         <div className="mt-7">
           <Comment
             threadId={thread.id}
-            currentUserImg={user.imageUrl}
+            currentUserImg={userInfo.image}
             currentUserId={JSON.stringify(userInfo._id)}
           />
         </div>
