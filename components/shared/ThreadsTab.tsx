90b36f69923e3d5f11219ae5eac9fb315a39f1d9
陶浩南 <14639548+haonantao-aaron@user.noreply.gitee.com>
2025-02-11 14:37:01 +0800

    fetch the profile threads build

--- components/shared/ThreadsTab.tsx
+++ components/shared/ThreadsTab.tsx
@@ -6,6 +6,7 @@ interface Props {
   accountType: string;
 }
 const ThreadsTab = async ({ currentUserId, accountId, accountType }: Props) => {
+  // fetch the profile threads
   return <div>ThreadsTab</div>;
 };
 
