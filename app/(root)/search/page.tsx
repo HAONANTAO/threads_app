37512bac97ebc39779c0baafb3432e11a0898934
陶浩南 <14639548+haonantao-aaron@user.noreply.gitee.com>
2025-02-11 15:48:46 +0800

    fetchUsers update

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { fetchUser } from "@/lib/actions/user.actions";
+import ProfileHeader from "@/components/shared/ProfileHeader";
 
-const page = () => {
+import { profileTabs } from "@/constants";
+import Image from "next/image";
+import ThreadsTab from "@/components/shared/ThreadsTab";
+const page = async () => {
+    const user = await currentUser();
+    // no user redirect to signin
+    if (!user) return null;
+  
+    // user information
+    const userInfo = await fetchUser(user.id);
+    if (!userInfo?.onboarded) redirect("/onboarding");
+  
+    // fetch users
+    
   return (
-    <div>page</div>
-  )
-}
+    <section>
+      <h1 className="head-text mb-10">Search</h1>
+    </section>
+  );
+};
 
-export default page
+export default page;
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,6 +4,7 @@ import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
 import Thread from "../models/thread.model";
+import { FilterQuery, SortOrder } from "mongoose";
 
 interface Params {
   userId: string;
@@ -82,5 +83,59 @@ export async function fetchUserPosts(userId: string) {
     return threads;
   } catch (error: any) {
     throw new Error(`fetch user post failed:${error.message}`);
+  }
+}
+
+export async function fetchUsers({
+  userId,
+  searchString = "",
+  pageNumber = 1,
+  pageSize = 20,
+  sortBy = "desc",
+}: {
+  userId: string;
+  searchString?: string;
+  pageNumber?: number;
+  pageSize?: number;
+  sortBy?: SortOrder;
+}) {
+  try {
+    connectToDB();
+    const skipAmount = (pageNumber - 1) * pageSize;
+
+    // insensitive
+    const regex = new RegExp(searchString, "i");
+
+    const query: FilterQuery<typeof User> = {
+      // not equal
+      id: { $ne: userId },
+    };
+
+    if (searchString.trim() !== "") {
+      // 表示“满足其中之一即可”。
+      query.$or = [
+        // 包含查询string
+        { username: { $regex: regex } },
+        { name: { $regex: regex } },
+      ];
+    }
+
+    const sortOptions = { createdAt: sortBy };
+
+    const usersQuery = User.find(query)
+      .sort(sortOptions)
+      .skip(skipAmount)
+      .limit(pageSize);
+
+    const totalUsersCount = await User.countDocuments(query);
+
+    const users = await usersQuery.exec();
+
+    // 还有
+    const isNext = totalUsersCount > skipAmount + users.length;
+
+    return { users, isNext };
+  } catch (error: any) {
+    throw new Error(`fetch user failed:${error.message}`);
   }
 }
