3cb177b97319bdca25ec7d96c849bf47f404c4c4
陶浩南 <14639548+haonantao-aaron@user.noreply.gitee.com>
2025-02-11 11:53:07 +0800

    Update page.tsx
    2025.02.11

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -3,34 +3,46 @@ import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { fetchUser } from "@/lib/actions/user.actions";
 import ProfileHeader from "@/components/shared/ProfileHeader";
-import { Tabs, TabsList,TabsContent,TabsTrigger} from "@/components/ui/tabs";
-
-
-const page = async ({params}:{params:{id:string}}) => {
+import { Tabs, TabsList, TabsContent, TabsTrigger } from "@/components/ui/tabs";
+import { profileTabs } from "@/constants";
+import Image from "next/image";
+const page = async ({ params }: { params: { id: string } }) => {
   const user = await currentUser();
-    // no user redirect to signin
-    if (!user) return null;
-  
-    // user information
-    const userInfo = await fetchUser(params.id);
-    
-    if (!userInfo?.onboarded) redirect("/onboarding");
-  
+  // no user redirect to signin
+  if (!user) return null;
+
+  // user information
+  const userInfo = await fetchUser(params.id);
+
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
   return (
     <>
       <section>
         <ProfileHeader
-        accountId={userInfo.id}
-        authUserId={user.id}
-        name={userInfo.name}
-        username={userInfo.username}
-        imgUrl={userInfo.image}
-        bio={userInfo.bio}/>
+          accountId={userInfo.id}
+          authUserId={user.id}
+          name={userInfo.name}
+          username={userInfo.username}
+          imgUrl={userInfo.image}
+          bio={userInfo.bio}
+        />
 
         <div className="mt-9">
-          <Tabs>
-            <TabsList>
-
+          <Tabs defaultValue="thread" className="w-full">
+            <TabsList className="tab">
+              {profileTabs.map((tab) => (
+                <TabsTrigger key={tab.label} value={tab.value}
+                className="tab">
+                  <Image 
+                  src={tab.icon}
+                  alt={tab.label}
+                  width={24}
+                  height={24}
+                  className="object-contain"/>
+                  <p className="max-sm:hidden">{tab.label}</p>
+                </TabsTrigger>
+              ))}
             </TabsList>
           </Tabs>
         </div>
