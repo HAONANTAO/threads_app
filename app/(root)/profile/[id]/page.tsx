d94ae5fea1e50c9fd9cd51ec13f01213ade57594
陶浩南 <14639548+haonantao-aaron@user.noreply.gitee.com>
2025-02-11 13:52:03 +0800

    save the threadStab

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -6,6 +6,7 @@ import ProfileHeader from "@/components/shared/ProfileHeader";
 import { Tabs, TabsList, TabsContent, TabsTrigger } from "@/components/ui/tabs";
 import { profileTabs } from "@/constants";
 import Image from "next/image";
+import ThreadsTab from "@/components/shared/ThreadsTab";
 const page = async ({ params }: { params: { id: string } }) => {
   const user = await currentUser();
   // no user redirect to signin
@@ -42,15 +43,28 @@ const page = async ({ params }: { params: { id: string } }) => {
                   />
 
                   <p className="max-sm:hidden">{tab.label}</p>
+
                   {/* number of threads */}
                   {tab.label === "Threads" && (
                     <p className="ml-1 rounded-sm bg-light-4 px-2 py-1 !text-tiny-medium text-light-2">
                       {userInfo.threads.length}
                     </p>
                   )}
+
                 </TabsTrigger>
               ))}
             </TabsList>
+            {profileTabs.map((tab)=>(
+              <TabsContent key={`content-${tab.label}`}
+              value={tab.value}
+              className="w-full text-light-1">
+                <ThreadsTab
+                currentUserId={user.id}
+                accountId={userInfo.id}
+                accountType="User"/>
+
+              </TabsContent>
+            ))}
           </Tabs>
         </div>
       </section>
---
+++ components/shared/ThreadsTab.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+interface Props {
+  currentUserId: string;
+  accountId: string;
+  accountType: string;
+}
+const ThreadsTab = async ({ currentUserId, accountId, accountType }: Props) => {
+  return <div>ThreadsTab</div>;
+};
+
+export default ThreadsTab;
