b6e3800beb69644f4a360354899b4313b8b76ec8
陶浩南 <14639548+haonantao-aaron@user.noreply.gitee.com>
2025-02-12 13:14:09 +0800

    getActivity function down

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -144,6 +144,26 @@ export async function fetchUsers({
 export async function getActivity(userId: string) {
   try {
     connectToDB();
+
+    //find all threads created by user
+    const userThreads = await Thread.find({ author: userId });
+
+    // collect all the child thread ids(replies) from the  "children" field！
+    const childThreadIds = userThreads.reduce((acc, userThread) => {
+      return acc.concat(userThread.children);
+    }, []);
+
+    // 是评论还不是作者自己的
+    const replies = await Thread.find({
+      _id: { $in: childThreadIds },
+      author: { $ne: userId },
+    }).populate({
+      path: "author",
+      model: User,
+      select: "name image _id",
+    });
+
+    return replies;
   } catch (error: any) {
     throw new Error(`getActivity failed:${error.message}`);
   }
