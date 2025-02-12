3653c54f4f1fd49d9b5bf440b0a4563bbdd42b85
陶浩南 <14639548+haonantao-aaron@user.noreply.gitee.com>
2025-02-12 12:19:35 +0800

    the user shows

--- components/cards/UserCard.tsx
+++ components/cards/UserCard.tsx
@@ -1,5 +1,10 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
+import { Button } from "../ui/button";
+import { useRouter } from "next/navigation";
+
 interface Props {
   id: string;
   name: string;
@@ -8,6 +13,7 @@ interface Props {
   personType: string;
 }
 const UserCard = ({ id, name, username, imgUrl, personType }: Props) => {
+  const router = useRouter();
   return (
     <article className="user-card">
       <div className="user-card_avatar">
@@ -18,7 +24,20 @@ const UserCard = ({ id, name, username, imgUrl, personType }: Props) => {
           height={48}
           className="rounded-full"
         />
+        {/* 缩略。。。 */}
+        <div className="flex-1 text-ellipsis">
+          <h4 className="text-base-semibold text-light-1">{name}</h4>
+          <p className="text-small-medium text-gray-1">@{username} </p>
+        </div>
       </div>
+
+      <Button
+        className="user-card_btn"
+        onClick={() => {
+          router.push(`/profile/${id} `);
+        }}>
+        View
+      </Button>
     </article>
   );
 };
