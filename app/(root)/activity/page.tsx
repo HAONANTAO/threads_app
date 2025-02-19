0c38e9d12888be93a8b83c175f62697f8be1ebf9
陶浩南 <14639548+haonantao-aaron@user.noreply.gitee.com>
2025-02-20 10:06:42 +1100

    activity replys
    activity replys

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -32,6 +32,12 @@ const page = async () => {
                     height={20}
                     className="rounded-full object-cover"
                   />
+                  <p className="!text-small-regular text-light-1">
+                    <span className="mr-1 text-primary-500">
+                      {comm.author.name}
+                    </span>
+                    replied to your thread
+                  </p>
                 </article>
               </Link>
             ))}
