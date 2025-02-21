2a89a90bd7d139effe5057e61058afd39734a345
陶浩南 <14639548+haonantao-aaron@user.noreply.gitee.com>
2025-02-21 18:25:51 +1100

    basic down the project

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,16 +1,20 @@
 import AccountProfile from "@/components/forms/AccountProfile";
+import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 async function Page() {
   const user = await currentUser();
-  
+  if (!user) return null;
+  const userInfo = await fetchUser(user.id);
+  if (userInfo?.onboarded) redirect("/");
   const userData = {
     id: user?.id || "",
-    objectId: "",
-    username: user?.username,
-    name:  user?.firstName || "",
-    bio:  "",
-    image:  user?.imageUrl,
+    objectId: userInfo._id,
+    username: userInfo? userInfo?.username : user?.username,
+    name: userInfo? userInfo?.name : user?.firstName || "",
+    bio: userInfo? userInfo?.bio :"",
+    image: userInfo? userInfo?.image : user?.imageUrl,
   };
   return (
     <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
