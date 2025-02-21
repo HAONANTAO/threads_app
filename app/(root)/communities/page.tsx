2db29e30dee1b9e6586b870ed9e81aabe0fc6259
陶浩南 <14639548+haonantao-aaron@user.noreply.gitee.com>
2025-02-21 15:59:21 +1100

    the community card

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,9 +1,49 @@
 import React from "react";
-
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+import Image from "next/image";
+import UserCard from "@/components/cards/UserCard";
+import { fetchCommunities } from "@/lib/actions/community.actions";
 const page = async () => {
+  const user = await currentUser();
+  // no user redirect to signin
+  if (!user) return null;
+
+  // user information
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  // fetchCommunities
+  const result = await fetchCommunities({
+    searchString: "",
+    pageNumber: 1,
+    pageSize: 25,
+  });
   return (
     <section>
-      <h1 className="head-text mb-10">communities</h1>
+      <h1 className="head-text mb-10">Search</h1>
+
+      {/* Search Bar */}
+
+      <div className="mt-14 flex flex-col gap-9">
+        {result.communities.length === 0 ? (
+          <p className="no-result">No users found</p>
+        ) : (
+          <>
+            {result.communities.map((comm) => (
+              <CommunityCard
+                key={comm.id}
+                id={comm.id}
+                name={comm.name}
+                username={comm.username}
+                imgUrl={comm.image}
+                personType="Community"
+              />
+            ))}
+          </>
+        )}
+      </div>
     </section>
   );
 };
