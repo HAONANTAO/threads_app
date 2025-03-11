187123d545395b3857c623d8576267ad36bfd658
陶浩南 <14639548+haonantao-aaron@user.noreply.gitee.com>
2025-03-11 17:27:23 +1100

    try to fix issue

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -23,7 +23,7 @@ const page = async ({ params }: { params: { id: string } }) => {
           <ThreadCard
             key={thread._id}
             id={thread._id}
-            currentUserId={user!.id}
+            currentUserId={user.id}
             parentId={thread.parentId}
             content={thread.text}
             author={thread.author}
@@ -46,7 +46,7 @@ const page = async ({ params }: { params: { id: string } }) => {
             <ThreadCard
               key={childItem._id}
               id={childItem._id}
-              currentUserId={childItem!.id}
+              currentUserId={user.id}
               parentId={childItem.parentId}
               content={childItem.text}
               author={childItem.author}
